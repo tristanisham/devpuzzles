e85201f3f25412d623c06694d0913c7ca0d6f50a
Tristan Isham <tristan@isham.co>
2024-02-20 19:12:36 -0500

    Updated to use modules

--- src/main.ts
+++ src/main.ts
@@ -1,17 +1,28 @@
 import express from "express";
 import process from "node:process";
+import morgan from "morgan";
+import { rateLimit } from "express-rate-limit"
 
-(async () => {
-    const app = express();
+// Server configuration starts
+const app = express();
+app.use(morgan('tiny'))
 
-    app.get("/", (_req, res): any => {
-        res.type("text/plain")
-        res.send("Hi gamer")
-    });
+const limiter = rateLimit({
+    windowMs: 15 * 60 * 1000, // 15 minutes
+    limit: 100, // Limit each IP to 100 requests per `window` (here, per 15 minutes).
+    standardHeaders: 'draft-7', // draft-6: `RateLimit-*` headers; draft-7: combined `RateLimit` header
+    legacyHeaders: false, // Disable the `X-RateLimit-*` headers.
+    // store: ... , // Use an external store for consistency across multiple server instances.
+});
+app.use(limiter);
 
-    if (process.env.NODE_ENV === "development" || process.env.NODE_ENV === undefined) {
-        console.log("http://localhost:3000")
-    }
+app.get("/", (_req, res): any => {
+    res.type("text/plain")
+    res.send("Hi gamer")
+});
 
-    app.listen(3000);
-})();
+if (process.env.NODE_ENV === "development" || process.env.NODE_ENV === undefined) {
+    console.log("http://localhost:3000")
+}
+
+app.listen(3000);
